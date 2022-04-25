4e64d96373bc4d2861253d065919002cae82e1be
Kevin Wang <kevinwang@kevinwang.us>
2022-04-24 22:33:17 -0400

    additional work

--- codingame/2022_spring_challenge/bronze.py
+++ codingame/2022_spring_challenge/bronze.py
@@ -13,7 +13,6 @@ CONTROL_RANGE = 2200
 SHIELD_RANGE = 2200
 WIND_RANGE = 1280
 BASE_RADIUS = 5000
-DAMAGE_RANGE = 800
 # base_x: The corner of the map representing your base
 base_x, base_y = [int(i) for i in input().split()]
 other_x, other_y = [17630-base_x, 9000-base_y]
@@ -29,25 +28,15 @@ opp_base_corners = [
     (other_x, other_y - 4600*flip),
     (other_x, other_y)
 ]
-homes = [
-    [(base_x + 7000*flip, base_y+1400*flip)],
-    [(base_x+1400*flip, base_y+7000*flip)],
-    [(base_x + 8000 * flip, base_y + 6000*flip)]
-]
-
-homes_2 = [
-    [(base_x+2000*flip, base_y+2000*flip)],
-    [(base_x + 5500*flip, base_y+1400*flip)],
-    [(base_x+1200*flip, base_y+5500*flip)],
+home = [
+    (base_x + 5500*flip, base_y+1400*flip),
+    (base_x+1200*flip, base_y+5500*flip),
+    (base_x + 8000 * flip, base_y + 6000*flip)
 ]
 foxhole_1_mid = (other_x - 3500 * flip, other_y - 3500 * flip)
 foxhole_1_left = (other_x - 5000 * flip, other_y - 1000 * flip)
 foxhole_1_right = (other_x - 1000 * flip, other_y - 5000 * flip)
-foxholes_2 = [
-    (other_x - 1400 * flip, other_y - 1400 * flip),
-    (other_x - 1300 * flip, other_y - 100 * flip),
-    (other_x - 100 * flip, other_y - 1300 * flip),
-]
+foxhole_2 = (other_x - 1400 * flip, other_y - 1400 * flip)
 
 def distance_from_position(entity, position):
     return math.dist((entity.x, entity.y), position)
@@ -97,7 +86,7 @@ class ActionType(Enum):
 class State:
     def __init__(self):
         self.timestep = 0
-        self.foxholes = [foxhole_1_mid]
+        self.foxhole = foxhole_1_mid
         self.should_shield_base = False
         self.opp_turtling = False
         self.which_opp_corner = 0
@@ -108,10 +97,10 @@ class State:
         self.which_opp_corner %= 3
     def set_foxhole(self):
         if self.opponent_danger > 50:
-            self.foxholes = foxholes_2
+            self.foxhole = foxhole_2
         else:
-            period = (self.timestep//8)%3
-            self.foxholes = [[foxhole_1_mid], [foxhole_1_right], [foxhole_1_left]][period]
+            period = (self.timestep//10)%3
+            self.foxhole = [foxhole_1_mid, foxhole_1_right, foxhole_1_left][period]
     def get_safe_distance(self):
         if self.opponent_in_my_base>0:
             return 6000
@@ -224,11 +213,11 @@ while True:
                 farming_bonus = extroversion[i] * delta/2000 * 50
                 attack_penalty = max(1-distance_from_opp_base(monster)/5000, 0.05)*400 if monster.threat_for==2 else 0
                 far_from_base_penalty = 100*base_love[i] if distance_from_base(monster) > state.get_safe_distance() else 0
-                on_it_penalty = 28 if len(actions_for_entity[monster.id]) > 0 else 0
+                on_it_penalty = 20 if len(actions_for_entity[monster.id]) > 0 else 0
                 action_scores[Action(ActionType.MOVE, target_id=monster.id, x=x, y=y)] += 90 * farm_love[i] - normalized_hero_distance * 25 + farming_bonus - attack_penalty - far_from_base_penalty - on_it_penalty
             # Control faraway monsters to attack
             if state.timestep > 100 and distance_from_base(monster)>4900 and  mana > 20 and distance_fn(monster, hero) < CONTROL_RANGE and monster.shield_life==0 and monster.threat_for!=2 and len(actions_for_entity[monster.id])==0:
-                health_bonus = monster.health*2 - 40
+                health_bonus = monster.health - 24
                 action_scores[Action(ActionType.CONTROL_TO_OPP, target_id=monster.id)] = 40 + min(mana-40, 35) + health_bonus
             # Wind monsters to attack
             if monster.shield_life==0 and BASE_RADIUS < distance_from_opp_base(monster) < BASE_RADIUS+2200 and distance_fn(hero, monster) < WIND_RANGE and mana > 20:
@@ -250,11 +239,11 @@ while True:
                 shield_penalty = monster.shield_life * 25
                 action_scores[Action(ActionType.SHIELD, target_id=monster.id)] += hp_bonus - shield_penalty - time_penalty + 10
 
+
         # Go fuck with opponent
         if state.timestep > 35:
-            homes[2] = state.foxholes
-            homes[0] = homes_2[0]
-            homes[1] = homes_2[1]
+            home[2] = state.foxhole
+            home[0] = (base_x+2000*flip, base_y+2000*flip)
         if state.timestep > 90:
             if mana < 20:
                 home_love[2] = 0
@@ -263,17 +252,8 @@ while True:
         #     x,y = target_towards((hero.x, hero.y), foxhole, 799)
         #     action_scores[Action(ActionType.MOVE, x=x, y=y)] += extroversion[i] * 100
         # Go home
-        for home in homes[i]:
-            x,y = target_towards((hero.x, hero.y), home, 799)
-            action_scores[Action(ActionType.MOVE, x=x, y=y)] += 8 * home_love[i]
-
-        for action in action_scores:
-            if action.type != ActionType.MOVE:
-                continue
-            for monster in monsters:
-                if monster.near_base==1 and monster.threat_for==2 and distance_from_position(monster, (action.x, action.y))<=DAMAGE_RANGE:
-                    action_scores[action] -= 35
-        
+        x,y = target_towards((hero.x, hero.y), home[i], 799)
+        action_scores[Action(ActionType.MOVE, x=x, y=y)] += 8 * home_love[i]
         print('top 4', file=sys.stderr, flush=True)
         for action, score in list(sorted(action_scores.items(), key=lambda x: x[1], reverse=True))[:4]:
             print(action, score, file=sys.stderr, flush=True)
